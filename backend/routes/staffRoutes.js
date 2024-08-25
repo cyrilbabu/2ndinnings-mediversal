e789c12115ce6b1f7a4764285c36ab8c5a44c7ec
Ayushman2694 <sahuayushman4@gmail.com>
2024-08-25 15:08:04 +0530

    patient schema

--- backend/routes/staffRoutes.js
+++ backend/routes/staffRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { login, staffSignup, updateStaff } from "../controllers/staff.controller.js";
+import { getAllStaff, login, staffSignup, updateStaff } from "../controllers/staff.controller.js";
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/signup", staffSignup);
 router.post("/updateStaff", updateStaff);
+router.get("/getAllStaff", getAllStaff);
 
 export default router;

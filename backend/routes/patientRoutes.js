6101881edda0efc9da1e5566a85cdea3a19e6eb7
Ayushman2694 <sahuayushman4@gmail.com>
2024-08-25 15:41:31 +0530

    fix patient routes

--- backend/routes/patientRoutes.js
+++ backend/routes/patientRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
+import { registerPatient, searchPatient } from "../controllers/patient.controller.js";
 
-import {registerPatient, searchPatient} from "./controllers/patient.controller.js"
+
 const router = express.Router();
 
 // Register a new patient
-router.post("/register", registerPatient);
+router.post("/register",registerPatient);
 
 // Search patients
-router.get("/search", searchPatient);
+router.get("/search",searchPatient);
 
 export default router;

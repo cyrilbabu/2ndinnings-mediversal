89f68aedc6915dc7c889339ae729a880c5cf502b
Ayushman2694 <sahuayushman4@gmail.com>
2024-08-25 12:34:22 +0530

    backend initial files

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+const port = 3000
+
+
+app.get("/",(req,res)=>{
+    return res.send("hello world")
+})
+
+app.listen(port,()=>{
+    console.log(`Server started on http://localhost:${port}`);
+});

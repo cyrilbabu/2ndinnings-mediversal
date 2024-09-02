13a69a9976da2300ffe5c9a56dea229294cabaec
Ayushman2694 <sahuayushman4@gmail.com>
2024-09-02 15:50:01 +0530

    assign care manager frontend

--- frontend/src/services/patient.js
+++ frontend/src/services/patient.js
@@ -21,3 +21,24 @@ export async function showAllPatient() {
     throw error;
   }
 }
+
+export async function assignCareManager(data) {
+  const apiUrl = `${url}/api/patient/getAllPatient`;
+
+  try {
+    const response = await axios.post(apiUrl,data);
+
+    if (response.status === 200) {
+      return response.data.allPatient; // Assuming response.data contains the array of admin details
+    } else {
+      console.error(
+        "Failed to fetch employee information:",
+        response.data.error
+      );
+      return null;
+    }
+  } catch (error) {
+    console.error("Error fetching employee information:", error);
+    throw error;
+  }
+}

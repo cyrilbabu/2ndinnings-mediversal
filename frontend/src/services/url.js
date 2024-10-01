7ae2eebcb28b53c710de98b9cc0d9681731744d2
cyril_babu <cyril123654789cyril@gmail.com>
2024-10-01 16:18:45 +0530

    gugu

--- frontend/src/services/url.js
+++ frontend/src/services/url.js
@@ -1,3 +1,3 @@
 const url = "http://localhost:6200";
-// const url = "https://twondinnings-mediversal-backend.onrender.com";
+// const url = "https://twondinnings-mediversal.onrender.com";
 export { url };
